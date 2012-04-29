492a8b52e2ef811f35a51f95493db3e1c7007441
Shahar Ronen <shahar.ronen@gmail.com>
2012-04-29 00:15:15 -0400

    added feature to get person GUID from links to relationship page; placed all searched relationships in a list instead of having them spread around the code; fixed iteration counter;  added some commments

--- freebase/darwin.py
+++ freebase/darwin.py
@@ -5,111 +5,180 @@ import metaweb        # Metaweb services
 Query for a person using his/her Freebase guid (default is Darwin's), and retrieve his/her properties
 and connections. 
 Properties retrived: name, date of birth, place of birth. 
-Yet to be retrieved: gender, places_lived, place_of_birth, profession, religion
+Yet to be retrieved: gender, places_lived, profession, religion
 Darwin's Freebase guid: #9202a8c04000641f800000000000cb7c
 '''
-def query_person_by_guid(person_guid='#9202a8c04000641f800000000000cb7c'):	
+
+# Create a session object
+freebase = metaweb.Session("api.freebase.com")
+
+# keys are relationship types returned by a person query;
+# some relationships link to relationship pages instead of person page;
+# values are the relationship type to look for in a relationship page and 
+# the name of the field containing the names of the two connected people.
+# NOTE: do not follow Influenecd and Influenced By connections.
+SEARCH_CONNECTIONS = {
+	"/people/person/parents" : {"relation_type": None, "fieldname": None},
+	"/people/person/sibling_s" : {"relation_type": "/people/sibling_relationship", "fieldname": "sibling"}, 
+	"/people/person/children" : {"relation_type": None, "fieldname": None}, 
+	"/people/person/spouse_s" : {"relation_type": "/people/marriage", "fieldname": "spouse"}, 
+	"/education/academic/advisors" : {"relation_type": None, "fieldname": None}, 
+	"/education/academic/advisees" : {"relation_type": None, "fieldname": None}, 
+	"/influence/influence_node/peers" : {"relation_type": "/influence/peer_relationship", "fieldname": "peers"},
+	#'/influence/influence_node/influenced': {"relation_type": None, "fieldname": None},
+	#'/influence/influence_node/influenced_by': {"relation_type": None, "fieldname": None}
+}
+
+def query_person_by_guid(person_guid='#9202a8c04000641f800000000000cb7c'):
 	# Our MQL query in Python: fields with value 'None' are to be populated
 	# by query results
-	query = {'type': '/people/person',
+	
+	# construct the query by starting with the property fields to look for...
+	query = {
+		'type': '/people/person',
 		'guid': person_guid,
 		'name': None,
 		'date_of_birth': None,
 		'place_of_birth': None,
-		'/influence/influence_node/influenced_by': [{'name':None, 'guid': None, 'optional': True}],
-		'/influence/influence_node/peers': [{'name':None, 'guid': None, 'optional': True}],
-		'/influence/influence_node/influenced': [{'name':None, 'guid': None, 'optional': True}],
-		"/people/person/parents": [{'name':None, 'guid': None, 'optional': True}],
-		"/people/person/sibling_s": [{'name':None, 'guid': None, 'optional': True}],
-		"/people/person/children": [{'name':None, 'guid': None, 'optional': True}],
-		"/people/person/spouse_s": [{'name':None, 'guid': None, 'optional': True}],
-		"/education/academic/advisors": [{'name':None, 'guid': None, "optional": True}],
-	    "/education/academic/advisees": [{'name':None, 'guid': None, "optional": True}],
 	}
 	
-	# Create a session object
-	freebase = metaweb.Session("api.freebase.com")
+	# ...now define the connection fields and add them
+	query_connections = { 
+		query_field_name: [{'name':None, 'guid': None, 'optional': True}] 
+		for query_field_name in SEARCH_CONNECTIONS.keys()
+	}
+	query.update(query_connections)
 	
 	# Submit query, get results
 	result = freebase.read(query)
+	if result==None:
+		print ">>> ERROR: No results for GUID %s; check GUID or type of this GUID" % person_guid
+		return None, None
 	
 	# Return properties
-	properties = extract_properties_from_results(result)
+	properties = _extract_properties_from_results(result)
 	#print properties # debug
 	
 	# Return connections
-	connections = extract_connections_from_results(result)
+	connections = _extract_connections_from_results(result)
 	#print connections # debug
 	
 	return connections, properties
 
 
-'''
-Return a dictionary of properties
-'''
-def extract_properties_from_results(results):
+def _find_relationship_target(relation_guid, relation_type, name_src):
+	'''
+	Given the GUID of relationship page and the source, find the target and
+	return its name and GUID.
+	E.g., for the relationship (Charles Darwin, Alfred Russel Wallace)
+	with Darwin as source, this function willl return Wallace's name and GUID.
+	TODO: find the GUID (scrape from page?) without using the name.  
+	'''
+	key_to_names_of_people_in_relationship = SEARCH_CONNECTIONS[relation_type]['fieldname']
+	
+	query = {
+		"guid" : relation_guid,
+		"type" : SEARCH_CONNECTIONS[relation_type]['relation_type'],
+		key_to_names_of_people_in_relationship : []
+	}
+	
+	# Submit query, get results
+	result = freebase.read(query)
+	if result==None:
+		return None, None
+	
+	# a relationship page contains two names; find the one different than the
+	# source. If no such name exists, return None, None
+	name_tgt = None
+	for name in result[key_to_names_of_people_in_relationship]: 
+		if name!=name_src:
+			name_tgt = name
+			break
+	if name_tgt is None:
+		return None, None
+	
+	# Now query for the target's name to find its guid.
+	query = [{
+		"name": name_tgt,
+		"type": "/people/person",
+		"guid": None
+	}]
+	result = freebase.read(query)
+	if len(result)!=1:
+		# No results, or too many results so we can't figure the correct person 
+		# print ">>> Too many %s (%s) !" % (name_tgt, len(result)) # debug
+		return None, None
+	
+	# All is well, return name and guid
+	return name_tgt, result[0]['guid'] # The only item on the list
+
+
+def _extract_properties_from_results(results):
+	'''
+	Return a dictionary of properties. Assume results is non-null.
+	'''
 	properties = {}
 	
-	if results:
-		# Use blank string instead of None - makes joining easier
-		properties['name'] =  normalize_strings(results['name'])
-		properties['date_of_birth'] = normalize_strings(results['date_of_birth'])
-		properties['place_of_birth'] = normalize_strings(results['place_of_birth'])
+	# Use blank string instead of None - makes joining easier
+	properties['name'] =  _normalize_strings(results['name']) # TODO: crashed here! on ID #9202a8c04000641f80000000002203ce
+	properties['date_of_birth'] = _normalize_strings(results['date_of_birth'])
+	properties['place_of_birth'] = _normalize_strings(results['place_of_birth'])
 	
 	return properties
 
 
-def normalize_strings(st):
+def _normalize_strings(st):
 	# Convert to utf-8 and replace None with a blank string to faciliate join()
 	return st.encode('utf-8') if st!=None else ''
 
-'''
-Return a list of tuples: (guid, relationship type)
-'''
-def extract_connections_from_results(results):
+
+def _extract_connections_from_results(results):
+	'''
+	Return a list containing guid, name, relationship type for each connection.
+	Assume results is non-null.
+	
+	Example:
+	{'conn_type': 'children',
+	 'guid': '#9202a8c04000641f80000000002f3ad8',
+	 'name': 'Charles Waring Darwin'},
+	'''
+	relationship_source_name = results['name']
+	
 	connections = []
 	
-	if results:
-		# Add connected people of all available relationship types
-		# to the list, adding the type of the relationship to each
-		# connection as well.
-		connections.extend( create_people_list(results,\
-			'/influence/influence_node/influenced_by') )
-		connections.extend( create_people_list(results,\
-			'/influence/influence_node/peers') )
-		connections.extend( create_people_list(results,\
-			'/influence/influence_node/influenced') )
-		connections.extend( create_people_list(results,\
-			'/people/person/parents') )
-		connections.extend( create_people_list(results,\
-			'/people/person/sibling_s') )
-		connections.extend( create_people_list(results,\
-			'/people/person/children') )
-		connections.extend( create_people_list(results,\
-			'/people/person/spouse_s') )
-		connections.extend( create_people_list(results,\
-			'/education/academic/advisors') )
-		connections.extend( create_people_list(results,\
-			'/education/academic/advisees') )
+	for field_name, values_list in results.iteritems():
+		if field_name not in SEARCH_CONNECTIONS.keys():
+			# field_names contain relationships and other properties,
+			# use only the former
+			continue
+			
+		relationship_type = field_name # easier to understand...
+		# Add connections of all sought relationship type to the list
+		for target in values_list:
+			if target['name']==None:
+				# Fix broken links: some Freebase links lead to a "relationship
+				# page" instead of a "person page", which results in the name of
+				# the target person showing up as None. However, a connection 
+				# page does list names and GUIDs of the two people connected in
+				# other fields -- this code tries to retrieve it.
+				## debug ##
+				# print ">>> Missing name for GUID %s " % target['guid']
+				# print ">>> Trying to locate using source %s" % relationship_source_name
+				
+				target['name'], target['guid'] = \
+					_find_relationship_target(target['guid'], relationship_type, \
+					relationship_source_name)
+				# print ">>> Found: %s %s (%s)" % (target['name'], \
+				#	target['guid'], relationship_type) # debug
+			
+			if target['name']!=None:
+				# If person already existed or was just found, add to list of
+				# connections and add the shortened relationship type
+				target['conn_type'] = relationship_type.split('/')[-1]
+				connections.append(target)
+				# print connections # debug
 			
 	return connections
-
-
-def create_people_list(result_dict, full_conn_type):
-	# get a result dictionary returned by our query and populate a list of
-	# people. Assume dictionary contains name and guid, and add type of
-	# connection (conn_type)
-		
-	list_of_people = []
-		
-	for dude in result_dict[full_conn_type]:
-		short_conn_type = full_conn_type.split('/')[-1]
-		#print "short conn type:", short_conn_type
-		dude['conn_type'] = short_conn_type
-		list_of_people.append(dude)
-		#print "dude's name:", dude
-		
-	return list_of_people
 
 
 def get_network_from_seed(seed_guid='#9202a8c04000641f800000000000cb7c', max_num_of_nodes=100):
@@ -120,7 +189,7 @@ def get_network_from_seed(seed_guid='#9202a8c04000641f800000000000cb7c', max_num
 	nodefile = open('nodefile.csv', 'w')
 	edgefile = open('edgefile.csv', 'w')
 	
-	search_set = [] # can't iterate a mutating set, must be a list
+	search_set = [] # can't iterate on a mutating set, must be a list
 	search_set.append(seed_guid) # Darwin's is #9202a8c04000641f800000000000cb7c
 	
 	try:
@@ -129,15 +198,20 @@ def get_network_from_seed(seed_guid='#9202a8c04000641f800000000000cb7c', max_num
 			# so we don't annoy Freebase.
 		
 			print "******" 			# debug
-			print "Iteration:", i, "guid:", person_guid, \
-				"Items in search set:",  len(search_set)	# debug
+			print "Iteration: %s guid: %s Items in search set: %s" \
+				% (i, person_guid, len(search_set))# debug
 			print "******" 			# debug
-			if max_num_of_nodes>0 and i>max_num_of_nodes:
-				# Place a hard limit on the number of nodes.
-				# 0 for unlimited 
-				break
 		
-			connection_list, properties = query_person_by_guid(person_guid)
+			try:
+				connection_list, properties = query_person_by_guid(person_guid)
+			except:
+				# problem with Query, move on
+				print "ERROR:", sys.exc_info()[0]
+				continue
+			
+			if not properties:
+				# Something went wrong, move on
+				continue
 			# print ">>>>>>>>", properties # debug
 			
 			# serialize properties to nodefile
@@ -147,20 +221,64 @@ def get_network_from_seed(seed_guid='#9202a8c04000641f800000000000cb7c', max_num
 			# serialize connections to edgefile
 			for connection in connection_list:
 				print connection['guid'], connection['name'], connection['conn_type']
-				#name = None if connection['name']==None else connection['name'].encode('utf-8')
-				name = normalize_strings(connection['name'])
+				name = _normalize_strings(connection['name'])
+				
 				
 				edgefile.write(person_guid + '\t' + properties['name'] + '\t' + \
 					connection['guid'] + '\t' + str(name) + '\t' + \
 					str(connection['conn_type']) + '\n') 
 				if connection['guid'] not in search_set:
 					search_set.append(connection['guid'])
-			
-				# TODO: add counter for number of connections
-	
-	finally:			
+					
+			if max_num_of_nodes>0 and i+1>=max_num_of_nodes:
+				# Place a hard limit on the number of nodes.
+				# 0 for unlimited TODO: 1 will actually go 
+				print "Stopping: limit set to %d iteration(s)" % max_num_of_nodes
+				break
+				
+	finally:
 		nodefile.close()
 		edgefile.close()
 	
 	return len(search_set)
 
+
+def convert_date_to_year(in_nodefile, out_nodefile):
+	'''
+	Convert the values in the date of birth column to years,
+	with optional range filter
+	'''
+	fin = open(in_nodefile, 'rU')
+	fout = open(out_nodefile, 'w')
+		
+	for line in fin:
+		cell_values = line[:-1].split('\t')
+		# the second cell holds the date.
+		# date format is yyyy-mm-dd, we only need the first
+		cell_values[1] = cell_values[1].split('-')[0]
+		fout.write('\t'.join(cell_values) + '\n')
+
+
+def filter_by_year(in_nodefile, out_nodefile, start_year=-10000, end_year=10000):
+	'''
+	Filter people by year. Must convert full dates to year first!
+	'''
+	fin = open(in_nodefile, 'rU')
+	fout = open(out_nodefile, 'w')
+	
+	print "Getting only people born between", start_year, "and", end_year
+	
+	for line in fin:
+		try:
+			# the second cell holds the year.
+			year = int(line.split('\t')[1])
+		except ValueError:
+			# No birth date
+			continue
+		
+		if year>=start_year and year<=end_year:
+			fout.write(line) # includes EOL
+			
+if __name__ == "__main__":
+	get_network_from_seed(seed_guid='#9202a8c04000641f800000000000cb7c', max_num_of_nodes=0)
+	
